b9349ddd4c255e196230f549b05253b92bd9885c
Halefom Alemu (Haniel) <hannelalex12@gmail.com>
2023-06-10 11:01:42 +0300

    adding the blog category

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -79,7 +79,30 @@ export default function Blog() {
 
   return (
     <div className="px-[2.5%] flex space-x-8">
-      <div className="w-[237px] flex-shrink-0"></div>
+      <div className="w-[237px] flex-shrink-0">
+        <div className="border rounded">
+          <div className="uppercase font-bold p-[15px] bg-[#f5f5f5] border-b">
+            blog category
+          </div>
+          <div className="py-[15px] px-[20px]">
+            {[
+              'our blog',
+              'demo category 1',
+              'demo category 2',
+              'demo category 3',
+              'demo category 4',
+              'demo category 5',
+            ].map((category, index) => (
+              <div
+                key={index}
+                className="capitalize text-xs py-[10px] px-[15px] cursor-pointer hover:text-primary"
+              >
+                {category}
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
       <div className="flex-auto space-y-6">
         <div className="space-y-8">
           <div className="uppercase font-bold">our blog</div>
