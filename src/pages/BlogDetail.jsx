f67c4920801b8df2e83a2928aeaf0ab120cde2a1
Halefom Alemu (Haniel) <hannelalex12@gmail.com>
2023-06-12 14:23:31 +0300

    adding the left side panel: the blog category navigation

--- src/pages/BlogDetail.jsx
+++ src/pages/BlogDetail.jsx
@@ -33,5 +33,33 @@ export default function BlogDetail() {
       'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lore...',
     comments: [],
   }
-  return <div className="px-[2.5%]">here we go with the blog details page</div>
+  return (
+    <div className="px-[2.5%]">
+      <div className="w-[237px] flex-shrink-0">
+        <div className="border rounded">
+          <div className="uppercase font-bold p-[15px] bg-[#f5f5f5] border-b">
+            blog category
+          </div>
+          <div className="py-[15px] px-[20px]">
+            {[
+              'our blog',
+              'demo category 1',
+              'demo category 2',
+              'demo category 3',
+              'demo category 4',
+              'demo category 5',
+            ].map((category, index) => (
+              <div
+                key={index}
+                className="capitalize text-xs py-[10px] px-[15px] cursor-pointer hover:text-primary"
+              >
+                {category}
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+      <div className="flex-auto space-y-6"></div>
+    </div>
+  )
 }
