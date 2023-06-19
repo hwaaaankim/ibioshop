6127c633a992a08305a83e3ca4bb60f2c1d5cb0e
Asmelash Abrha <asmelashabrha15@gmail.com>
2023-06-19 08:51:48 -0500

    Fix links

--- src/components/product/Breadcrumb.jsx
+++ src/components/product/Breadcrumb.jsx
@@ -15,7 +15,7 @@ function Breadcrumb() {
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-3 h-3 mx-2.5">
           <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
         </svg>
-        <li><a href="#" className='hover:text-[#094bad]'>Smartphone & Tablets</a></li>
+        <li><a href="category" className='hover:text-[#094bad]'>Smartphone & Tablets</a></li>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-3 h-3 mx-2.5">
           <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
         </svg>
