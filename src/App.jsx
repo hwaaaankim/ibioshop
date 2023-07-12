56045ec08516d58ad40203f70fa5969f62fd1c1b
Halefom Alemu (Haniel) <hannelalex12@gmail.com>
2023-07-12 03:24:19 +0300

    formating applied

--- src/App.jsx
+++ src/App.jsx
@@ -66,7 +66,6 @@ function App() {
           <Route path="blog" element={<Blog />} />
           <Route path="blog-detail" element={<BlogDetail />} />
 
-          <Route path="go-responsive" element={<div></div>} />
           <Route path="*" element={<NotFound />} />
         </Routes>
         {!footer.hidden && <Footer />}
