7328c9640b6be5cc034888fa27b3c13c8d0ec98c
Halefom Alemu (Haniel) <hannelalex12@gmail.com>
2023-07-19 01:35:15 +0300

    changed my mind and implemented the http api calls in another way

---
+++ src/services/http.js
@@ -0,0 +1,40 @@
+import apiService from './http-common'
+import { errorHandler } from './httpErrorHandler'
+
+export const http = {
+  request: async (payload) => {
+    try {
+      const response = await apiService.request(payload)
+      return response ? response.data : response
+    } catch (error) {
+      const errorModified = errorHandler(error)
+      return { isError: true, error: errorModified }
+    }
+  },
+  upload: async (payload) => {
+    payload = {
+      ...payload,
+      headers: { 'Content-Type': 'multipart/form-data' },
+    }
+    try {
+      const response = await apiService.request(payload)
+      return response ? response.data : response
+    } catch (error) {
+      const errorModified = errorHandler(error)
+      return { isError: true, error: errorModified }
+    }
+  },
+  download: async (payload) => {
+    payload = {
+      ...payload,
+      responseType: 'blob',
+    }
+    try {
+      const response = await apiService.request(payload)
+      return response ? response.data : response
+    } catch (error) {
+      const errorModified = errorHandler(error)
+      return { isError: true, error: errorModified }
+    }
+  },
+}
