22712456e049bdedb9026efc4dc829c091bc23bd
Halefom Alemu (Haniel) <hannelalex12@gmail.com>
2023-08-05 17:27:28 +0300

    adding cart slice to rtk

---
+++ src/store/slices/cartSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState: [],
+  reducers: {
+    addToCart(state, { payload: product }) {
+      const existingItem = state.find((item) => item.id === product.id)
+      if (existingItem) {
+        existingItem.quantity++
+      } else {
+        state.push({ ...product, quantity: 1 })
+      }
+    },
+    removeFromCart(state, { payload: productId }) {
+      const index = state.findIndex((item) => item.id === productId)
+      if (index !== -1) {
+        state.splice(index, 1)
+      }
+    },
+  },
+})
+
+export const { addToCart, removeFromCart } = cartSlice.actions
+
+export default cartSlice.reducer
