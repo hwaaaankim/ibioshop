5470eb83e6b0a31d9e61fe35b5daff6ef89c49ed
Halefom Alemu (Haniel) <hannelalex12@gmail.com>
2023-08-17 14:19:34 +0300

    adding the checkboxes

--- src/components/product/ProductPreview.jsx
+++ src/components/product/ProductPreview.jsx
@@ -226,9 +226,9 @@ function ProductPreview({ product }) {
               </div>
             </div>
           </div>
-          <div className="mt-4">
+          <div className="mt-2 space-y-3">
             <h4 className="my-1 text-[#666] font-medium">Available Options</h4>
-            <div className="text-xs leading-6">
+            <div className="text-xs leading-6 space-y-2">
               <span className="flex items-center">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -242,9 +242,7 @@ function ProductPreview({ product }) {
                     clip-rule="evenodd"
                   />
                 </svg>
-                <label className="text-[#666] mt-1 mr-2.5 font-normal">
-                  Colors
-                </label>
+                <label className="text-[#666] mr-2.5 font-normal">Colors</label>
               </span>
               <ul className="flex space-x-1">
                 <li className="w-6 h-6 cursor-pointer p-[2px] border">
@@ -267,6 +265,36 @@ function ProductPreview({ product }) {
                   />
                 </li>
               </ul>
+            </div>
+
+            <div className="text-xs leading-6 space-y-2">
+              <span className="flex items-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  className="w-2 h-2 text-red-600 mr-1"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
+                    clip-rule="evenodd"
+                  />
+                </svg>
+                <label className="text-[#666] mt-1 mr-2.5 font-normal">
+                  Checkbox
+                </label>
+              </span>
+              <div className="space-y-2">
+                {[1, 2, 3, 4].map((item) => (
+                  <div className="flex space-x-2 items-center">
+                    <input type="checkbox" />
+                    <div className="text-sm text-gray-700">
+                      Checkbox 1 (+$12.00)
+                    </div>
+                  </div>
+                ))}
+              </div>
             </div>
 
             <div className="flex w-full mb-4 mt-2">
